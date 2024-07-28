e99adc7fc4d9ceb47ed8fc216d7629cf414a9d94
Lost-in-Noise <nancykikvadze@gmail.com>
2024-07-28 21:53:49 +0400

    Enhance: Add Responsiveness to Recommendations Section and Hide Scroll Top Button on Small Screens

---
+++ js/script3.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// accordion
+document.addEventListener("DOMContentLoaded", () => {
+  const headers = document.querySelectorAll(".accordion-header");
+
+  headers.forEach((header) => {
+    header.addEventListener("click", () => {
+      const content = header.nextElementSibling;
+      const arrow = header.querySelector(".accordion-arrow");
+
+      if (content.style.display === "block") {
+        content.style.display = "none";
+        arrow.classList.remove("up");
+      } else {
+        document.querySelectorAll(".accordion-content").forEach((item) => {
+          item.style.display = "none";
+        });
+        document.querySelectorAll(".accordion-arrow").forEach((arrowIcon) => {
+          arrowIcon.classList.remove("up");
+        });
+
+        content.style.display = "block";
+        arrow.classList.add("up");
+      }
+    });
+  });
+});
+
+// scroll top btn
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+  if (window.innerWidth > 780) {
+    if (
+      document.body.scrollTop > 100 ||
+      document.documentElement.scrollTop > 100
+    ) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
